8ae0aff21a21ed8a62e0b16471f2386991ddc8da
Bartosz Kownacki <bkownacki@man.poznan.pl>
2021-10-21 12:07:16 +0200

    Bug fix

--- app/models/ACSocketStateModel/models.py
+++ app/models/ACSocketStateModel/models.py
@@ -2,17 +2,16 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 from database import Base
 
-class Config(Base):
-    __tablename__ = "AcSocket"
+class AcSocket(Base):
+    __tablename__ = "acSockets"
 
     id = Column(Integer, primary_key=True, index=True)
-    state: Column(Boolean)
-    timeStampToTurnOn: Column(DateTime, nullable=True)
-    timeStampToTurnOff: Column(DateTime, nullable=True)
-    createdDate: Column(DateTime)
-    createdById: Column(Integer, ForeignKey("users.id"))
-    
+    state = Column(Boolean)
+    timeStampToTurnOn = Column(DateTime, nullable=True)
+    timeStampToTurnOff = Column(DateTime, nullable=True)
+    createdDate = Column(DateTime)
+    owner_id = Column(Integer, ForeignKey("users.id"))
 
-    User = relationship("User", back_populates="ac_socket")
+    owner = relationship("User", back_populates="acSockets")
 
 
--- app/models/UserModel/models.py
+++ app/models/UserModel/models.py
@@ -11,4 +11,4 @@ class User(Base):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
-    ac_socket = relationship("AcSocket", back_populates="user")
+    acSockets = relationship("AcSocket", back_populates="owner")
